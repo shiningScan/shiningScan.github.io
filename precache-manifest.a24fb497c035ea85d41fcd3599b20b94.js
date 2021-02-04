da111c507a4feb330db90096bff6a7d75d1ca95f
shiningWolf <403644529@qq.com>
2021-02-05 07:27:50 +0800

    静态文件上传

---
+++ precache-manifest.a24fb497c035ea85d41fcd3599b20b94.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "379007755ccef46011e0",
+    "url": "/css/about.b3b0204e.css"
+  },
+  {
+    "revision": "379007755ccef46011e0",
+    "url": "/js/about.358e1343.js"
+  },
+  {
+    "revision": "31aed579316d47d0c887",
+    "url": "/css/app.11a55255.css"
+  },
+  {
+    "revision": "31aed579316d47d0c887",
+    "url": "/js/app.14e2c5de.js"
+  },
+  {
+    "revision": "1e355567fec8a2d0f0f4",
+    "url": "/js/chunk-2d21a3d1.092d93b8.js"
+  },
+  {
+    "revision": "8538b13b14a3414ff688",
+    "url": "/js/chunk-8c70688c.a9d3c137.js"
+  },
+  {
+    "revision": "7033818062026f7d3976",
+    "url": "/css/chunk-vendors.e7ecfd74.css"
+  },
+  {
+    "revision": "7033818062026f7d3976",
+    "url": "/js/chunk-vendors.3784d3e1.js"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "11c0b1e464dc27a1ab4e61edb790e409",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
